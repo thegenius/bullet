c5df679cddcd7b8d405398b46e3fc58766bf8171
wangwei <soulww@163.com>
2019-12-03 12:58:59 +0800

    add luban.rb

---
+++ luban.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Luban < Formula
+  desc "luban project generator"
+  homepage "https://github.com/thegenius/bullet"
+  url "https://github.com/thegenius/bullet/archive/v0.2.6.zip"
+  sha256 "cf753cafc180333882c9f72c22cc80a04878a4b5615c14740cfee84f67e0e315"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install "distribute/darwin/0.2.5/luban"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test luban`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
